aeb2f66ed06e323f4b6a02c92682641c4c420efa
asnow <asnow.dev@gmail.com>
2012-06-08 18:50:18 +0400

    init remove

--- lib/juggernaut.rb
+++ lib/juggernaut.rb
@@ -1,7 +1,7 @@
-require 'rubygems'
 require 'logger'
 require 'eventmachine'
 require 'json'
+require 'action_view'
 $:.unshift(File.dirname(__FILE__))
 
 
@@ -157,3 +157,26 @@ require 'juggernaut/message'
 require 'juggernaut/client'
 require 'juggernaut/server'
 require 'juggernaut/runner'
+
+
+require File.join(File.dirname(__FILE__), './lib/juggernaut')
+require File.join(File.dirname(__FILE__), './lib/juggernaut/j_rails')
+require File.join(File.dirname(__FILE__), './lib/juggernaut/j_rails/helpers')
+
+Juggernaut.send(:include, Juggernaut::JRails::ConvenienceMethods)
+ActionView::Base.send(:include, Juggernaut::JRails::Helpers)
+
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :juggernaut => ['juggernaut/swfobject', 'juggernaut/juggernaut']
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :juggernaut_jquery => ['juggernaut/json', 'juggernaut/juggernaut', 'juggernaut/jquerynaut', 'juggernaut/swfobject']
+
+ActionController::Base.class_eval do
+  alias_method :render_without_juggernaut, :render
+  include Juggernaut::JRails::RenderExtension
+  alias_method :render, :render_with_juggernaut
+end
+
+ActionView::Base.class_eval do
+  alias_method :render_without_juggernaut, :render
+  include Juggernaut::JRails::RenderExtension
+  alias_method :render, :render_with_juggernaut
+end
